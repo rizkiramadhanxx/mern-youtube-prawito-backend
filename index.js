0f2a131e13b3144886e468ebdf52fb3a63b86632
Rizki Ramadhan <rizkiramdhanx@gmail.com>
2022-04-25 06:12:09 +0700

    27

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require("express");
-
 const app = express();
+const bodyParser = require("body-parser");
 
-app.use(() => {
-  console.log("hello server");
+const authRoutes = require("./src/routes/auth");
+const blogRoutes = require("./src/routes/blog");
+
+// app.use(express.json());
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET",
+    "PUT",
+    "POST",
+    "DELETE",
+    "OPTIONS",
+    "PATCH"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
 });
+
+app.use("/v1/auth", authRoutes);
+app.use("/v1/blog", blogRoutes);
 
 app.listen(4000);
