6c901c304ef48ce9848540be4a0d12f472f2f62c
Rizki Ramadhan <rizkiramdhanx@gmail.com>
2022-04-30 09:14:09 +0700

    last

--- index.js
+++ index.js
@@ -40,14 +40,12 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET",
-    "PUT",
-    "POST",
-    "DELETE",
-    "OPTIONS",
-    "PATCH"
+    "GET, POST,PUT, PATCH, DELETE, OPTIONS"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Authorization,X-Requested-With,Accept"
+  );
   next();
 });
 
